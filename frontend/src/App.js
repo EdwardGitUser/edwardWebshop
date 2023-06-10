ea9f0bbf9ab33b693b548e0f74b5d08b2f5c7ec1
Edward Pasenyuk <edwarddominator@gmail.com>
2023-06-10 16:47:41 +0300

    Before adding webtokens, checkpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Headere from './components/Headere';
+import Footer from './components/Footer';
 
 const App = () => {
-    return <h1>App</h1>;
+    return (
+        <>
+            <Headere />
+            <main className='py-3'>
+                <Container>
+                    <Outlet />
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
 };
 
 export default App;
